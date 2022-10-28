72faff7a9ffa51701e3eef0800553f3b6ad5d593
Théo <contentin.theo@gmail.com>
2022-10-28 13:20:01 +0200

    Premier commit

---
+++ TP1.py
@@ -0,0 +1,6 @@
+import pandas as pd # Pour lire les fichiers
+import numpy as np # Pour effectuer des calculs mathématiques
+import matplotlib.pyplot as plt # Pour réaliser des graphiques
+from scipy import stats # Pour des calculs statistiques
+
+tableau = pd.read_table("painting.txt")
