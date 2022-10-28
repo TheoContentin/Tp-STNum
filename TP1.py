34b4a1219e72a234731cc14eba4c923b153d9fe0
Théo <contentin.theo@gmail.com>
2022-10-28 14:00:35 +0200

    Q1 et Q2

--- TP1.py
+++ TP1.py
@@ -3,4 +3,20 @@ import numpy as np # Pour effectuer des calculs mathématiques
 import matplotlib.pyplot as plt # Pour réaliser des graphiques
 from scipy import stats # Pour des calculs statistiques
 
-tableau = pd.read_table("painting.txt")
+tableau8 = pd.read_table("painting8.txt",
+                        sep=";",
+                        decimal=".",
+                        header=None)
+
+tableau8[7] = 1-tableau8.sum(1)
+
+#Q1 : Il faut bien calculer la dernière colonne pour avoir toutes les composantes
+print(tableau8.std(0))
+
+
+
+plt.subplots(figsize=(8, 6))
+
+tableau8.boxplot()
+plt.show()
+
