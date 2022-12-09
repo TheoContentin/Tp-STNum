28064eb7a90914a096482447b776acb326b6c131
Florian Gilbertas <florian.gilbertas@eleves.enpc.fr>
2022-12-09 14:33:15 +0100

    Partie 1 avec interprétation

--- TP3.py
+++ TP3.py
@@ -22,6 +22,27 @@ reglin_sim_model = ols('dist ~ speed', data = cars)
 reglin_sim = reglin_sim_model.fit()
 print(reglin_sim.summary())
 
+# Analyse des résidus #
+cars['residu'] = reglin_sim.resid
+
+moy_res = cars.residu.mean()
+print(f'Moyenne des résidus : {moy_res:.2f}')
+
+var_res = cars.residu.var()
+print(f'Variance des résidus : {var_res:.2f}')
+
+plt.subplots()
+plt.boxplot(cars['residu'], labels=['Boîte à moustaches'])
+plt.grid()
+plt.ylabel('Résidu')
+plt.title('Régression linéaire simple')
+
+plt.subplots()
+plt.hist(cars['residu'], density=True)
+plt.xlabel('Résidu')
+plt.ylabel('Histogramme')
+plt.title('Régression linéaire simple')
+
 ## Q3 ##
 plt.subplots(figsize=(8, 6))
 plt.scatter(cars['speed'], cars['dist'])
@@ -42,8 +63,13 @@ def prevision(vit):
 print('prévision de la distance d arrêt pour une vitesse de O mph : '+str(prevision(0))+' m \n')
 print('prévision de la distance d arrêt pour une vitesse de 40 mph : '+str(prevision(40))+' m \n')
 
+a_prevoir = pd.DataFrame({'speed': [40]})
+prev = reglin_sim.predict(a_prevoir)
+print(f'Valeur prévue : {prev[0]:.2f}')
+
 #### 2 ####
 state = pd.read_table("state.txt",
                         sep=" ",
                         decimal=".")
 
+## Q5 ##
